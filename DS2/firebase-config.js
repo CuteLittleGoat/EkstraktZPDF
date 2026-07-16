69198591db9ce5ea59777bfbe50d88d2857bf800
CuteLittleGoat <taryf@o2.pl>
2026-07-16 10:14:20 +0200

    Add Firebase configuration for DS2 checklist

---
+++ DS2/firebase-config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = Object.freeze({
+  apiKey: "AIzaSyA4m8ZkI-L9MYgKcbAtt8gJaTtr2TlCN5s",
+  authDomain: "audiorpg-2eb6f.firebaseapp.com",
+  projectId: "audiorpg-2eb6f",
+  storageBucket: "audiorpg-2eb6f.firebasestorage.app",
+  messagingSenderId: "848367463263",
+  appId: "1:848367463263:web:500bdf5c394ac786e2e4ea"
+});
